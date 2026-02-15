fcfa3989498e67a690010b2fec32341e7958f896
rahamottulla <rahamottullam@gmail.com>
2026-02-15 23:51:15 +0530

    remove unused API_BASE_URL import

--- frontend/src/shared/api/axios.js
+++ frontend/src/shared/api/axios.js
@@ -1,9 +1,9 @@
 // src/shared/api/axios.js
 import axios from "axios";
-import { API_BASE_URL, API_TIMEOUT } from "./config";
+import { API_TIMEOUT } from "./config";
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: "",
   timeout: API_TIMEOUT,
   headers: {
     "Content-Type": "application/json",
