585ef59fedea6dc4704c7ebd5412e209806933f9
rahamottulla <rahamottullam@gmail.com>
2026-02-21 22:33:13 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      ssl: true, // ensure SSL/TLS handshake for MongoDB Atlas
-       // TEMPORARY: bypass certificate validation
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
