fcc9c52521e35edfb15f5efa7d0e4a5372d00bc9
rahamottulla <rahamottullam@gmail.com>
2026-02-24 16:45:47 +0530

    update

--- backend/features/auth/socialAuth.routes.js
+++ backend/features/auth/socialAuth.routes.js
@@ -3,7 +3,6 @@ import express from "express";
 import passport from "passport";
 
 const router = express.Router();
-const isProduction = process.env.NODE_ENV === "production";
 
 // GOOGLE AUTH 
 // Step 1: Redirect user to Google for authentication
@@ -20,8 +19,8 @@ router.get(
     const { token } = req.user;
     res.cookie("token", token, {
       httpOnly: true,
-      secure: isProduction,    // true only in production
-      sameSite: isProduction ? "None" : "Lax", // because frontend + backend different domain
+      secure: true,   
+      sameSite: "None", 
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
@@ -44,8 +43,8 @@ router.get(
     const { token } = req.user;
     res.cookie("token", token, {
       httpOnly: true,
-      secure: isProduction,
-      sameSite: isProduction ? "None" : "Lax",
+      secure: true,
+      sameSite: "None",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
@@ -54,11 +53,11 @@ router.get(
 );
 
 // LOGOUT 
-router.get("/logout", (req, res) => {
+router.get("/logout", (_req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? "None" : "Lax",
+    secure: true,
+    sameSite: "None",
   });
 
   res.redirect(`${process.env.FRONTEND_URL}/login`);
